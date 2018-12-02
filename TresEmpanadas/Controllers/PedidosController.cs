ab950a16a4c9852fe11c5d7bf69f9c997ebbf8fc
Maximiliano De Pietro <elwindwalker@gmail.com>
2018-12-02 16:06:53 -0300

    Purgar usuarios para invitar + [WIP] validaciones front

--- TresEmpanadas/Controllers/PedidosController.cs
+++ TresEmpanadas/Controllers/PedidosController.cs
@@ -21,7 +21,8 @@ namespace TresEmpanadas.Controllers
             {
                 //System.Web.HttpContext.Current.Session["IdUsuario"] = 1;
                 ViewBag.gustosEmpanadas = servicioPedido.ListarGustosEmpanadas();
-                ViewBag.usuariosDisponibles = servicioUsuario.ListarUsuarios();
+                var usuariosDisponibles = servicioUsuario.ListarUsuariosParaInvitar((int)Session["idUsuario"]);
+                ViewBag.usuariosDisponibles = usuariosDisponibles;
                     if (idPedido == null)
                     {
                         ViewBag.conModelo = false;
@@ -176,7 +177,7 @@ namespace TresEmpanadas.Controllers
                 {
                     ViewBag.opciones = servicioPedido.CargarOpciones();
                     ViewBag.gustosEmpanadas = servicioPedido.ListarGustosEmpanadas();
-                    ViewBag.usuariosDisponibles = servicioUsuario.ListarUsuarios();
+                    ViewBag.usuariosDisponibles = servicioUsuario.ListarUsuariosParaInvitar((int)Session["idUsuario"]);
                     ViewBag.usuariosInvitados = servicioPedido.UsuariosInvitados(idPedido);
                     ViewBag.conModelo = true;
                     int idParametro = (int)idPedido;
---
+++ TresEmpanadas/Scripts/Pedidos/validar-pedido.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+let formElements = {
+    invitadosMultiselect: document.getElementById('js-prueba')
+}
+
+function validateForm() {
+    valid = false;
+    console.log(formElements.invitadosMultiselect.value);
+    return valid;
+}
+
+
+function validateMultiSelect() {
+    if (formElements.invitadosMultiselect.value === '') {
+
+    }
+}
