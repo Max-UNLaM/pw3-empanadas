0a7249f41be8c559a1aeaad6fb9d7ee1a8c27497
Maximiliano De Pietro <elwindwalker@gmail.com>
2018-12-02 22:11:25 -0300

    Validaciones front en carga y edición de pedido

--- TresEmpanadas/Scripts/Pedidos/validar-pedido.js
+++ TresEmpanadas/Scripts/Pedidos/validar-pedido.js
@@ -1,18 +1,34 @@
 ﻿"use strict";
 
 let formElements = {
-    invitadosMultiselect: document.getElementById('js-prueba')
+    invitadosMultiselect: document.getElementById('js-prueba'),
+    invitadosError: document.getElementById('invitados-error'),
+    selectGustos: document.getElementById('selectGustos'),
+    selectGustosError: document.getElementById('selectGustos-error')
 }
 
 function validateForm() {
-    valid = false;
-    console.log(formElements.invitadosMultiselect.value);
+    let valid = false;
+    resetErrors();
+    validateMultiSelect();
     return valid;
 }
 
+function resetErrors() {
+    let mensajesDeError = document.getElementsByClassName('val-error-message');
+    for (let i = 0; i < mensajesDeError.length; i++) {
+        mensajesDeError[i].classList.add('val-hidden');
+    }
+}
 
 function validateMultiSelect() {
     if (formElements.invitadosMultiselect.value === '') {
+        formElements.invitadosError.classList.remove('val-hidden')
+    }
+}
 
+function validateGustos() {
+    if (formElements.selectGustos.value === '') {
+        formElements.selectGustosError.classList.remove('val-hidden')
     }
 }
